ea1a4158421c7ccbee82318580be479bff1dc5f1
hiromps <akihiro19970324@gmail.com>
2026-03-28 22:57:56 +0900

    Use Auth0 domain option

--- lib/auth0.ts
+++ lib/auth0.ts
@@ -1,13 +1,10 @@
 import { Auth0Client } from "@auth0/nextjs-auth0/server"
 
-const issuerBaseURL =
-  process.env.AUTH0_ISSUER_BASE_URL ?? (process.env.AUTH0_DOMAIN ? `https://${process.env.AUTH0_DOMAIN}` : undefined)
-
 export const auth0 = new Auth0Client({
+  domain: process.env.AUTH0_DOMAIN,
   appBaseUrl: process.env.APP_BASE_URL,
   clientId: process.env.AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
-  issuerBaseURL,
   secret: process.env.AUTH0_SECRET,
 })
 
