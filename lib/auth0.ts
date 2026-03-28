f50c760f86b9e04209e8f3c8c7090f413fa39e3f
hiromps <akihiro19970324@gmail.com>
2026-03-28 22:53:41 +0900

    Configure Auth0 client explicitly

--- lib/auth0.ts
+++ lib/auth0.ts
@@ -1,6 +1,15 @@
 import { Auth0Client } from "@auth0/nextjs-auth0/server"
 
-export const auth0 = new Auth0Client()
+const issuerBaseURL =
+  process.env.AUTH0_ISSUER_BASE_URL ?? (process.env.AUTH0_DOMAIN ? `https://${process.env.AUTH0_DOMAIN}` : undefined)
+
+export const auth0 = new Auth0Client({
+  appBaseUrl: process.env.APP_BASE_URL,
+  clientId: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  issuerBaseURL,
+  secret: process.env.AUTH0_SECRET,
+})
 
 export function getAuth0Client() {
   return auth0
